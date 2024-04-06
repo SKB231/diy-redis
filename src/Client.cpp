55f07ec050b09ad4ebbaa395b8dbc4d1fbc4b756
Krishna <sbhat97@gatech.edu>
2024-04-06 13:16:09 -0400

    basic 2nd stage code

---
+++ src/Client.cpp
@@ -0,0 +1,44 @@
+#include <arpa/inet.h>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  std::cout << "Hello World";
+
+  int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (client_fd < 0) {
+    std::cerr << "Failed to create server socket\n";
+    return 1;
+  }
+
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(6379);
+
+  std::cout << "\nAttemtping to connect to server" << std::endl;
+  int res =
+      connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (res < 0) {
+    std::cout << "Failed to connect client to server";
+  }
+
+  std::cout << "Connected to server. Enter your message: ";
+
+  std::string message;
+  std::cin >> message;
+  std::cout << message.c_str();
+
+  send(client_fd, (void *)message.c_str(), message.size(), 0);
+
+  close(client_fd);
+
+  return 0;
+}
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,12 +1,16 @@
 #include <arpa/inet.h>
+#include <cerrno>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <netdb.h>
+#include <ostream>
 #include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+void handle(int socket_fd, struct sockaddr *client_addr);
 
 int main(int argc, char **argv) {
   // You can use print statements as follows for debugging, they'll be visible
@@ -49,14 +53,51 @@ int main(int argc, char **argv) {
 
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
+  while (true) {
+    std::cout << "Waiting for a client to connect...\n";
 
-  std::cout << "Waiting for a client to connect...\n";
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                           (socklen_t *)&client_addr_len);
+    if (client_fd < 0) {
+      std::cout << "Socket connection failed\n" << std::endl;
 
-  accept(server_fd, (struct sockaddr *)&client_addr,
-         (socklen_t *)&client_addr_len);
-  std::cout << "Client connected\n";
-
-  close(server_fd);
+      continue;
+    }
+    std::cout << "Client connected\n";
+    handle(client_fd, (struct sockaddr *)&client_addr);
+  }
 
   return 0;
+}
+
+void handle(int socket_fd, struct sockaddr *client_addr) {
+  char buff[32] = {};
+  std::string final_message = "";
+  while (true) {
+    std::cout << "Listening for message: " << std::endl;
+    int data_written = recv(socket_fd, buff, 32, 0);
+    std::cout << "Recieved chunk of size " << data_written << "\n";
+    if (data_written == -1) {
+      std::cout << errno << "\n";
+      break;
+    }
+    for (char i : buff) {
+      final_message += i;
+    }
+    std::cout << "Message at the moment: \n" << final_message << std::endl;
+    if (data_written < 32) {
+      break;
+    }
+  }
+  std::cout << "Recieved message: " << final_message;
+
+  if (final_message.size() > 6) {
+    int message_len = final_message.size();
+    std::string pingCmd = final_message.substr(message_len - 6, 4);
+    if (pingCmd == "ping") {
+      std::string resp = std::string("+PONG\r\n");
+      send(socket_fd, (void *)resp.c_str(), resp.size(), 0);
+      std::cout << "sending message " << resp;
+    }
+  }
 }
